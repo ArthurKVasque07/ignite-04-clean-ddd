79354bddba710e98a1364cb6b1a277dccbf2e936
Arthur Kavagouth Vasque <arthurkvasque.eng@outlook.com>
2023-05-21 09:54:44 -0300

    feat: add Delete question comment

--- src/tests/repositories/in-memory-question-comments-repository.ts
+++ src/tests/repositories/in-memory-question-comments-repository.ts
@@ -6,6 +6,24 @@ export class InMemoryQuestionCommentRepository
 {
   public items: QuestionComment[] = [];
 
+  async findById(id: string): Promise<QuestionComment | null> {
+    const comment = this.items.find((item) => item.id.toString() === id);
+
+    if (!comment) {
+      return null;
+    }
+
+    return comment;
+  }
+
+  async delete(questionComment: QuestionComment) {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === questionComment.id
+    );
+
+    this.items.splice(itemIndex, 1);
+  }
+
   async create(questionComment: QuestionComment) {
     this.items.push(questionComment);
   }
