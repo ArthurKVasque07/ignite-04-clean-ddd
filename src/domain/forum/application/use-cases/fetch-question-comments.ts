d5c1f81f80d6b5dfdd717a5f7b04c3ce80f14d72
Arthur Kavagouth Vasque <arthurkvasque.eng@outlook.com>
2023-05-22 08:10:34 -0300

    feat: add fetch question comments

---
+++ src/domain/forum/application/use-cases/fetch-question-comments.ts
@@ -0,0 +1,31 @@
+import { QuestionComment } from "../../enterprise/entities/question-comment";
+import { IQuestionCommentsRepository } from "../repositories/question-comments-repository";
+
+interface FetchQuestionCommentUseCaseRequest {
+  questionId: string;
+  page: number;
+}
+
+interface FetchQuestionCommentUseCaseResponse {
+  questionComments: QuestionComment[];
+}
+
+export class FetchQuestionCommentsUseCase {
+  constructor(
+    private _questionCommentsRepository: IQuestionCommentsRepository
+  ) {}
+
+  async execute({
+    questionId,
+    page,
+  }: FetchQuestionCommentUseCaseRequest): Promise<FetchQuestionCommentUseCaseResponse> {
+    const questionComments =
+      await this._questionCommentsRepository.findManyByQuestionId(questionId, {
+        page,
+      });
+
+    return {
+      questionComments,
+    };
+  }
+}
