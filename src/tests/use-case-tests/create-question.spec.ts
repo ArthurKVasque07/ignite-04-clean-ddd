cad7a49db2d3c24b88e0926b3f793c0c5b378711
Arthur Kavagouth Vasque <arthurkvasque.eng@outlook.com>
2023-06-28 09:25:44 -0300

    feat: add Attachment Question

--- src/tests/use-case-tests/create-question.spec.ts
+++ src/tests/use-case-tests/create-question.spec.ts
@@ -1,5 +1,6 @@
 import { CreateQuestionUseCase } from "@/domain/forum/application/use-cases/create-question";
 import { InMemoryQuestionsRepository } from "../repositories/in-memory-questions-repository";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 
 // sut - system under test
 
@@ -17,11 +18,17 @@ describe("Create question", () => {
       authorId: "1",
       title: "title",
       content: "Conteudo da pergunta",
+      attachmentsIds: ["1", "2"],
     });
 
     expect(result.isRight()).toBe(true);
     expect(inMemoryQuestionsRepository.items[0]).toEqual(
       result.value?.question
     );
+    expect(inMemoryQuestionsRepository.items[0].attachments).toHaveLength(2);
+    expect(inMemoryQuestionsRepository.items[0].attachments).toEqual([
+      expect.objectContaining({ attachmentId: new UniqueEntityID("1") }),
+      expect.objectContaining({ attachmentId: new UniqueEntityID("2") }),
+    ]);
   });
 });
